9ce1ea4f337c12e55540d4fc23933ddcffed47ca
jaccooos <38593287+jaccooos@users.noreply.github.com>
2018-06-05 19:35:05 +0200

    localhost

--- Myadmin/config.inc.php
+++ Myadmin/config.inc.php
@@ -28,7 +28,7 @@ $i++;
 /* Authentication type */
 $cfg['Servers'][$i]['auth_type'] = 'cookie';
 /* Server parameters */
-$cfg['Servers'][$i]['host'] = 'sql7.freesqldatabase.com';
+$cfg['Servers'][$i]['host'] = 'localhost';
 $cfg['Servers'][$i]['compress'] = false;
 $cfg['Servers'][$i]['AllowNoPassword'] = false;
 
